1b79982a91c0a9054471e931c945e11d465ff669
bharath kollareddy <bkollareddy@gmail.com>
2017-03-05 00:57:38 -0500

    changes to create employee and get employee

--- src/main/java/com/eis/employeeapp/EmployeeController.java
+++ src/main/java/com/eis/employeeapp/EmployeeController.java
@@ -1,7 +1,5 @@
 package com.eis.employeeapp;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,11 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.eis.employeeapp.models.Address;
-import com.eis.employeeapp.models.EmployeeDetails;
 import com.eis.employeeapp.models.EmployeeMaster;
 import com.eis.employeeapp.repositories.EmployeeMasterRepository;
 
@@ -24,35 +22,12 @@ public class EmployeeController
 	private EmployeeMasterRepository employeeMasterRepository;
 
 	private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
-
-	@RequestMapping("/createEmployee")
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/createEmployee")
 	@ResponseBody
-	public EmployeeMaster createEmployee() {
+	public EmployeeMaster createEmployee(@RequestBody EmployeeMaster employeeMaster) {
 		try {
-			EmployeeMaster employeeMaster=new EmployeeMaster();
-			employeeMaster.setFirstName("bharath");
-			employeeMaster.setLastName("Kollareddy");
-			employeeMaster.setCreatedBy("employeeapp");
-			SimpleDateFormat format= new SimpleDateFormat("MMddyyyy");
-			employeeMaster.setCreatedDate(format.format(new Date()));
-			
-			EmployeeDetails employeeDetails= new EmployeeDetails();
-			employeeDetails.setDateOfBirth(format.format(new Date()));
-			employeeDetails.setSsn(123);
-			employeeDetails.setRole("Employee");
-			employeeDetails.setDateOfJoining(format.format(new Date()));
-			employeeMaster.setEmployeeDetails(employeeDetails);
-			
-			Address address= new Address();
-			address.setAddressLine1("1100 west chester pike");
-			address.setAddressLine2("apt j07");
-			address.setCity("west chester");
-			address.setState("PA");
-			address.setCountry("usa");
-			employeeMaster.setAddress(address);
-			
 			employeeMasterRepository.save(employeeMaster);
-			log.info("Record created::"+employeeMaster.getEmpid());
 			return employeeMaster;
 		}
 		catch (Exception ex) {
@@ -61,7 +36,7 @@ public class EmployeeController
 		}
 	}
 	
-	@RequestMapping("/getEmployeeByLastName/{lastName}")
+	@RequestMapping(method = RequestMethod.GET, value ="/getEmployeeByLastName/{lastName}")
 	@ResponseBody
 	public List<EmployeeMaster> getEmployee(@PathVariable("lastName") String lastName){
 		List<EmployeeMaster> employees= employeeMasterRepository.findByLastName(lastName);

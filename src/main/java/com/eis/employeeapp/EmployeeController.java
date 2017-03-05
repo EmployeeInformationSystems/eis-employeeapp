872a0427a4fe446838f2cf7df431314534a3214e
bharath kollareddy <bkollareddy@gmail.com>
2017-03-04 21:39:49 -0500

    code to fetch employee using last name

--- src/main/java/com/eis/employeeapp/EmployeeController.java
+++ src/main/java/com/eis/employeeapp/EmployeeController.java
@@ -2,11 +2,13 @@ package com.eis.employeeapp;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,7 +27,7 @@ public class EmployeeController
 
 	@RequestMapping("/createEmployee")
 	@ResponseBody
-	public void createEmployee() {
+	public EmployeeMaster createEmployee() {
 		try {
 			EmployeeMaster employeeMaster=new EmployeeMaster();
 			employeeMaster.setFirstName("bharath");
@@ -51,9 +53,19 @@ public class EmployeeController
 			
 			employeeMasterRepository.save(employeeMaster);
 			log.info("Record created::"+employeeMaster.getEmpid());
+			return employeeMaster;
 		}
 		catch (Exception ex) {
 			log.error("something went wrong::"+ex);
+			return null;
 		}
+	}
+	
+	@RequestMapping("/getEmployeeByLastName/{lastName}")
+	@ResponseBody
+	public List<EmployeeMaster> getEmployee(@PathVariable("lastName") String lastName){
+		List<EmployeeMaster> employees= employeeMasterRepository.findByLastName(lastName);
+		System.out.println("size::"+employees.size());
+		return employees;
 	}
 }
